a6df89fd96a3de5fda9310a2205f5fbf99e91a0f
Richard Volkov <richardvolkov@Richards-MBP.Dlink>
2024-06-11 15:50:14 +0300

    Replace bcrypt with bcryptjs

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Change this line
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
